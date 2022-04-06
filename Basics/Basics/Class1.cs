f897ddc638b73b87a2b56ce0df88ec9ffa293b0d
Gustavo Pereira <xgustavux@hotmail.com>
2022-04-07 09:48:30 +1000

    studying the Basics of C#

---
+++ Basics/Basics/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    //declare the enum
+    public enum ShippingMethod
+    {
+        Regular = 1,
+        Registered = 2,
+        Express = 3,
+    }
+    internal class enumClass
+    {
+        static public void getShippingMethod()
+        {
+            var method1 = ShippingMethod.Registered;
+            Console.WriteLine(method1);
+            var method2 = 3;
+            //casting number equivalent into a ShippingMethod
+            Console.WriteLine((ShippingMethod)method2);
+
+            //parsing string that matches a enum type
+            var method3 = "Regular";
+            var newMethod3 = Enum.Parse(typeof(ShippingMethod), method3);
+
+            Console.WriteLine(newMethod3);
+        }
+    }
+}
---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Basics 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //casting
+
+            //if convertions won't lose data, and types are compatible, you CAN convert.
+            byte b = 1; //byte datatype has 1 byte
+            int c = b; //int datatype has 4 bytes
+
+            //if might lose data, it won't convert implicitly, you have to CAST.
+            int d = 1;
+            byte e = (byte)d;
+
+            //converting uncompatible data types
+            string s = "1";
+            int x = int.Parse(s);
+            //or
+            int y = Convert.ToInt32(s);
+
+            //joining array items
+            var arrayOfInt = new int[5] { 1,2,3,4,5 };
+            string list = string.Join(", ", arrayOfInt);
+
+            //Console.WriteLine(list);
+
+            //Verbatim strings
+            string msg = "Check this url: /n /t http:///path//path//file";
+            string verbatimMsg = @"Check this url:
+    http://path/path/file";
+
+            //Console.WriteLine($"msg: {msg}, verbatimMsg: {verbatimMsg}");
+
+            //Enums
+            //enumClass.getShippingMethod();
+        }
+
+      
+    }
+}
