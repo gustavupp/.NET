d2e030f3cdcd30803a8966d9cf949dc181741d24
Gustavo Pereira <xgustavux@hotmail.com>
2022-04-08 21:39:26 +1000

    create action of todoApp

---
+++ TodoApp/TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        static public List<TodoItem> todoList = new List<TodoItem>();
+        public IActionResult Index()
+        {
+            return View(todoList);
+        }
+
+        public IActionResult Create()
+        {
+            var newTodo = new TodoItem();
+            return View(newTodo);
+        }
+
+        public IActionResult CreateTodo(TodoItem todo)
+        {
+            todoList.Add(todo);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TodoApp/TodoApp/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string TodoName { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
